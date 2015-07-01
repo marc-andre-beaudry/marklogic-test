75201c2e8df9c03effea5cbaea05d124b81a9d86
Marc-Andre Beaudry <marc-andre.beaudry@usherbrooke.ca>
2015-07-01 11:23:00 -0400

    Updated generate_test_data.js script + query

--- generate_test_data.js
+++ generate_test_data.js
@@ -1,55 +1,42 @@
 // Generate Test Triples
 // Model is the following : Rack -> CommRoom -> Floor -> Building -> City -> Country -> Region
+// Rack is located on a comm  room, comm room is located on a floor...
 
+var namespace       = "http://www.marc.com";
+var regionCount     = 4;
+var countryCount    = 75;
+var cityCount       = 300;
+var buildingCount   = 1000;
+var floorCount      = 2000;
+var commRoomCount   = 5000;
+var rackCount       = 20000;
+
+// Delete all triples from database
 function clearDb() {
   sem.sparqlUpdate("DELETE where { ?s ?p ?o }");
 }
 
-function generateGeneric(subjectName, count, hasChild, childName, childCount) {
-  for(var i = 0; i < count; i++) {
-    var type = "<Class#" + subjectName + ">";
-    var subject = "<" + subjectName + "#" + i + ">";
-    var predicate = "<Attribute#Code>";
-    var object = genericAttribute + subjectName + i + "\"";
-    
-    var typeQuery = "INSERT DATA { " + subject + " a " + type + " }";
-    var attributeQuery = "INSERT DATA { " + subject + " " + predicate + " " + object + " }";
-    sem.sparqlUpdate(typeQuery);
-    sem.sparqlUpdate(attributeQuery);
+// Generate data and create a located on relationship to a child subject.
+function generateData(className, classCount, hasChild, childClassName, childClassCount) {
+  for(var classId = 0; classId < classCount; classId++) {
+    var typeUri = "<" + namespace + "/" + "classes#" + className + ">";
+    var classNameUri = "<" + namespace + "/" + className + "#" + classId + ">";    
+    var query = "INSERT DATA { " + classNameUri + " a " + typeUri + " }";
+    sem.sparqlUpdate(query);
     if(hasChild == true) {
-      var childObject = "<" + childName + "#" + (i % childCount) + ">";
-      var relationshipQuery = "INSERT DATA { " + subject + " " + locatedOnPredicate + " " + childObject + " }";
+      var childClassNameUri = "<" + namespace + "/" + childClassName + "#" + (classId % childClassCount) + ">";
+      var locatedOnUri = "<" + namespace + "/" + "Relationships#Located_on" + ">"; 
+      var relationshipQuery = "INSERT DATA { " + classNameUri + " " + locatedOnUri + " " + childClassNameUri + " }";
       sem.sparqlUpdate(relationshipQuery);
     }
   }    
 }
 
-var type = "rdf:type";
-var locatedOnPredicate = "<Relationships#Located_on>";
-var genericPredicate = "<Attribute#Name>";
-var genericAttribute = "\"Name=";
-
-var regionCount     = 4;
-var countryCount    = 50;
-var cityCount       = 200;
-var buildingCount   = 500;
-var floorCount      = 1000;
-var commRoomCount   = 2000;
-var rackCount       = 10000;
-
-//var regionCount     = 4;
-//var countryCount    = 8;
-//var cityCount       = 16;
-//var buildingCount   = 32;
-//var floorCount      = 64;
-//var commRoomCount   = 128;
-//var rackCount       = 256;
-
 clearDb();
-generateGeneric("Region", regionCount, false, "", "");
-generateGeneric("Country", countryCount, true, "Region", regionCount);
-generateGeneric("City", cityCount, true, "Country", countryCount);
-generateGeneric("Building", buildingCount, true, "City", cityCount);
-generateGeneric("Floor", floorCount, true, "Building", buildingCount);
-generateGeneric("CommRoom", commRoomCount, true, "Floor", floorCount);
-generateGeneric("Rack", rackCount, true, "CommRoom", commRoomCount);
+generateData("Region", regionCount, false, "", "");
+generateData("Country", countryCount, true, "Region", regionCount);
+generateData("City", cityCount, true, "Country", countryCount);
+generateData("Building", buildingCount, true, "City", cityCount);
+generateData("Floor", floorCount, true, "Building", buildingCount);
+generateData("CommRoom", commRoomCount, true, "Floor", floorCount);
+generateData("Rack", rackCount, true, "CommRoom", commRoomCount);
